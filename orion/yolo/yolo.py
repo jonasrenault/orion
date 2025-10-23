a0bbdb4eb6c44ebc7869e3ca4b3d1fb5e429fafc
Jonas Renault <jonasrenault@gmail.com>
2025-10-23 12:05:34 +0200

    update yolo train and predict. Add train notebook

--- orion/yolo/yolo.py
+++ orion/yolo/yolo.py
@@ -3,7 +3,8 @@ from pathlib import Path
 from typing import Annotated
 
 import typer
-from ultralytics import YOLO
+from ultralytics import YOLO  # pyright: ignore[reportPrivateImportUsage]
+from ultralytics.engine.results import Results
 
 from orion.config.settings import settings
 
@@ -26,52 +27,86 @@ def train(
     ] = settings.ORION_HOME_DIR
     / "dataset"
     / "dataset.yaml",
+    output: Annotated[
+        Path, typer.Option("--output", "-o", help="save directory.")
+    ] = Path()
+    / "runs",
+    exist_ok: Annotated[bool, typer.Option(help="override results in save dir.")] = False,
     epochs: Annotated[int, typer.Option("--epochs", "-e", help="epochs.")] = 60,
     imgsz: Annotated[int, typer.Option("--imgsz", "-i", help="image size.")] = 640,
     batch: Annotated[int, typer.Option("--batch", "-b", help="batch size.")] = 16,
-    device: Annotated[str, typer.Option(help="device.")] = "cpu",
+    device: Annotated[str, typer.Option(help="device.")] = "",
+    plots: Annotated[bool, typer.Option(help="plot metrics during training.")] = True,
 ):
     """
-    Train a Yolo base model with data in the data directory.
+    Fine-tune a base Yolo model on given dataset.
 
     Args:
         base_model (str | Path): base model name.
         data (Path): training data.
+        output (Path, optional): save directory. Defaults to Path() / runs.
+        exist_ok (bool, optional): override results in save dir if exists.
+            Defaults to False.
         epochs (int, optional): epochs. Defaults to 60.
         imgsz (int, optional): image size. Defaults to 640.
         batch (int, optional): batch size. Defaults to 16.
-        device (str, optional): device to use. Defaults to 'cpu'.
+        device (str, optional): device to use. Defaults to ''.
+        plots (bool, optional): plot metrics during training. Defaults to True.
     """
     model = YOLO(base_model)  # load a pretrained model
+
     results = model.train(
-        data=data, epochs=epochs, imgsz=imgsz, batch=batch, device=device
+        data=data,
+        epochs=epochs,
+        imgsz=imgsz,
+        batch=batch,
+        device=device,
+        project=str(output),
+        exist_ok=exist_ok,
+        plots=plots,
     )
     return results
 
 
+@app.command()
 def predict(
-    model_path: str | Path,
-    source: str | Path,
-    save_txt: bool = True,
-    save_conf: bool = True,
-):
+    model_path: Annotated[
+        Path,
+        typer.Argument(help="model path.", file_okay=True, exists=True),
+    ],
+    data: Annotated[
+        Path,
+        typer.Argument(
+            help="data to make predictions on.",
+            file_okay=True,
+            dir_okay=True,
+            exists=True,
+        ),
+    ],
+    save_txt: Annotated[
+        bool, typer.Option(help="save detection results in a txt file.")
+    ] = True,
+    save_conf: Annotated[
+        bool, typer.Option(help="save confidence score for each detection.")
+    ] = True,
+) -> list[Results]:
     """
     Run prediction on a source using the given model
 
     Args:
-        model_path (str | Path): the model to use for prediction
-        source (str | Path): the source of data to predict
+        model_path (str | Path): the model to use for prediction.
+        data (str | Path): data to make predictions on.
         save_txt (bool, optional): save detection results in a txt file. Defaults to True.
         save_conf (bool, optional): save confidence score for each detection.
             Defaults to True.
 
     Returns:
-        list: A list of detection results.
+        list[Results]: A list of detection results.
     """
     model = YOLO(model_path)
 
     # Run inference on the source
-    results = model.predict(source, stream=False, save_txt=save_txt, save_conf=save_conf)
+    results = model.predict(data, stream=False, save_txt=save_txt, save_conf=save_conf)
     return results
 
 
