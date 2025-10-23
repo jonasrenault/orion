8ad07c1b207ff691fb3e92095d750c3464a74718
Jonas Renault <jonasrenault@gmail.com>
2025-10-23 15:17:42 +0200

    update tracking

--- orion/yolo/yolo.py
+++ orion/yolo/yolo.py
@@ -33,9 +33,12 @@ def train(
     / "dataset"
     / "dataset.yaml",
     output: Annotated[
-        Path, typer.Option("--output", "-o", help="save directory.")
-    ] = Path()
-    / "runs",
+        Path,
+        typer.Option(
+            "--output", "-o", file_okay=False, dir_okay=True, help="save directory."
+        ),
+    ] = Path.cwd()
+    / "runs/train",
     exist_ok: Annotated[bool, typer.Option(help="override results in save dir.")] = False,
     epochs: Annotated[int, typer.Option("--epochs", "-e", help="epochs.")] = 60,
     imgsz: Annotated[int, typer.Option("--imgsz", "-i", help="image size.")] = 640,
@@ -58,19 +61,25 @@ def train(
         device (str, optional): device to use. Defaults to ''.
         plots (bool, optional): plot metrics during training. Defaults to True.
     """
+    LOGGER.info(f"Loading model from {base_model}...")
     yolo_settings.update({"tensorboard": True})
-    model = YOLO(base_model)  # load a pretrained model
+    model = YOLO(base_model)
+    project = output.parent
+    name = output.name
 
+    LOGGER.info(f"Running training. Output saved to [bold green]{output}[/].")
     results = model.train(
         data=data,
         epochs=epochs,
         imgsz=imgsz,
         batch=batch,
         device=device,
-        project=str(output),
+        project=project,
+        name=name,
         exist_ok=exist_ok,
         plots=plots,
     )
+    LOGGER.info(f"Training complete. Output saved to [bold green]{output}[/].")
     return results
 
 
@@ -95,9 +104,16 @@ def predict(
     save_conf: Annotated[
         bool, typer.Option(help="save confidence score for each detection.")
     ] = True,
+    output: Annotated[
+        Path,
+        typer.Option(
+            "--output", "-o", file_okay=False, dir_okay=True, help="save directory."
+        ),
+    ] = Path.cwd()
+    / "runs/predict",
 ) -> list[Results]:
     """
-    Run prediction on a source using the given model
+    Run predictions on a set of images using the given model.
 
     Args:
         model_path (str | Path): the model to use for prediction.
@@ -105,23 +121,57 @@ def predict(
         save_txt (bool, optional): save detection results in a txt file. Defaults to True.
         save_conf (bool, optional): save confidence score for each detection.
             Defaults to True.
+        output (Path, optional): Output directory.
+            Defaults to Path.cwd() / "runs/predict".
 
     Returns:
         list[Results]: A list of detection results.
     """
+    LOGGER.info(f"Loading model from {model_path}...")
     model = YOLO(model_path)
+    project = output.parent
+    name = output.name
 
-    # Run inference on the source
-    results = model.predict(data, stream=False, save_txt=save_txt, save_conf=save_conf)
+    LOGGER.info(f"Running prediction on {data}. Output saved to [bold green]{output}[/].")
+    results = model.predict(
+        data,
+        stream=False,
+        save_txt=save_txt,
+        save_conf=save_conf,
+        project=project,
+        name=name,
+    )
+    LOGGER.info(f"Predictions complete. Output saved to [bold green]{output}[/].")
     return results
 
 
+@app.command()
 def track(
-    model_path: str | Path,
-    source: str | Path,
-    conf: float = 0.5,
-    save: bool = True,
-    tracker: str | Path = "botsort.yaml",
+    model_path: Annotated[
+        Path,
+        typer.Argument(help="model path.", file_okay=True, exists=True),
+    ],
+    data: Annotated[
+        Path,
+        typer.Argument(
+            help="input video.",
+            file_okay=True,
+            exists=True,
+        ),
+    ],
+    conf: Annotated[
+        float, typer.Option("--conf", "-c", help="confidence threshold for detections.")
+    ] = 0.5,
+    tracker: Annotated[
+        str, typer.Option("--tracker", "-t", help="tracker configuration file.")
+    ] = "botsort.yaml",
+    output: Annotated[
+        Path,
+        typer.Option(
+            "--output", "-o", file_okay=False, dir_okay=True, help="save directory."
+        ),
+    ] = Path.cwd()
+    / "runs/track",
 ):
     """
     Track tanks in a video using a YOLO model and specified tracker.
@@ -130,15 +180,29 @@ def track(
         model_path (str | Path): Path to the YOLO model weights file.
         source (str | Path): Path to the source of video to track
         conf (float, optional): Confidence threshold for detections . Defaults to 0.5.
-        save (bool, optional): Save the processed video with tracked tanks.
-            Defaults to True.
         tracker (str | Path, optional): The tracker configuration file.
             Defaults to "botsort.yaml".
+        output (Path, optional): Output directory. Defaults to Path.cwd() / "runs/track".
 
     Returns:
         results: The tracking results, typically including information on
             detected and tracked tanks.
     """
+    LOGGER.info(f"Loading model from {model_path}...")
     model = YOLO(model_path)
-    results = model.track(source=source, conf=conf, save=save, tracker=tracker)
-    return results
+    project = output.parent
+    name = output.name
+
+    LOGGER.info(f"Running tracking on {data}. Output saved to [bold green]{output}[/].")
+    results = model.track(
+        source=data,
+        conf=conf,
+        tracker=tracker,
+        save=True,
+        stream=True,
+        project=project,
+        name=name,
+        exist_ok=True,
+    )
+    list(results)
+    LOGGER.info(f"Tracking complete. Output saved to [bold green]{output}[/].")
